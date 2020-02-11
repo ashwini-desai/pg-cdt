459c4235ad08185b3fb85c2f49736da16c15ef36
Ashwini Desai <ashwinidesai2310@gmail.com>
2020-02-11 17:46:55 +0530

    Test with table jsonb column having a json array of phone_numbers

--- src/test/kotlin/pg/cdt/JsonbTest.kt
+++ src/test/kotlin/pg/cdt/JsonbTest.kt
@@ -6,6 +6,7 @@ import io.kotlintest.TestCase
 import io.kotlintest.TestResult
 import io.kotlintest.extensions.TopLevelTest
 import io.kotlintest.matchers.collections.shouldContainAll
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
 import norm.executeCommand
 import norm.executeQuery
@@ -39,7 +40,7 @@ class JsonbTest : DescribeSpec() {
     }
 
     init {
-        describe("with one phone number as json object") {
+        describe("phone_numbers as json object") {
 
             it("without index") {
                 dataSource.connection.use {
@@ -55,8 +56,8 @@ class JsonbTest : DescribeSpec() {
                     it.executeQuery("EXPLAIN ANALYZE SELECT phone_numbers from contacts;").toList()
                 }
 
-                println("Plan time = " + queryAnalysis[1])
-                println("Execution time = " + queryAnalysis[2])
+                println("JSON_OBJECT_WITHOUT_INDEX: Plan time = " + queryAnalysis[1])
+                println("JSON_OBJECT_WITHOUT_INDEX: Execution time = " + queryAnalysis[2])
 
                 result.map { it["phone_numbers"] } shouldContainAll listOf("{\"Home\": 8899776612}", "{\"Work\": 9876543210}")
             }
@@ -74,10 +75,92 @@ class JsonbTest : DescribeSpec() {
                     it.executeQuery("EXPLAIN ANALYZE SELECT phone_numbers from contacts;").toList()
                 }
 
-                println("Plan time = " + queryAnalysis[1])
-                println("Execution time = " + queryAnalysis[2])
+                println("JSON_OBJECT_WITH_INDEX: Plan time = " + queryAnalysis[1])
+                println("JSON_OBJECT_WITH_INDEX: Execution time = " + queryAnalysis[2])
 
                 result.map { it["phone_numbers"] } shouldContainAll listOf("{\"Home\": 8899776612}", "{\"Work\": 9876543210}")
+            }
+        }
+
+        describe("phone_numbers as json array") {
+            it("without index") {
+                dataSource.connection.use {
+                    val sqlFile = this::class.java.classLoader.getResource("contacts_with_multiple_phone_numbers.sql").readText().trim()
+                    it.executeCommand("DROP INDEX idx_phone_numbers;")
+                    it.executeCommand("DELETE FROM contacts;")
+                    it.executeCommand(sqlFile)
+                }
+
+                val result = dataSource.connection.use {
+                    it.executeQuery("SELECT phone_numbers from contacts;").toList()
+                }
+
+                val queryAnalysis = dataSource.connection.use {
+                    it.executeQuery("EXPLAIN ANALYZE SELECT phone_numbers from contacts;").toList()
+                }
+
+                println("JSON_ARRAY_WITHOUT_INDEX: Plan time = " + queryAnalysis[1])
+                println("JSON_ARRAY_WITHOUT_INDEX: Execution time = " + queryAnalysis[2])
+
+                result.map { it["phone_numbers"] }.size shouldBe 20
+            }
+
+            it("with index") {
+                dataSource.connection.use {
+                    it.executeCommand("CREATE INDEX idx_phone_numbers ON contacts USING gin (phone_numbers);")
+                }
+
+                val result = dataSource.connection.use {
+                    it.executeQuery("SELECT phone_numbers from contacts;").toList()
+                }
+
+                val queryAnalysis = dataSource.connection.use {
+                    it.executeQuery("EXPLAIN ANALYZE SELECT phone_numbers from contacts;").toList()
+                }
+
+                println("JSON_ARRAY_WITH_INDEX: Plan time = " + queryAnalysis[1])
+                println("JSON_ARRAY_WITH_INDEX: Execution time = " + queryAnalysis[2])
+
+                result.map { it["phone_numbers"] }.size shouldBe 20
+            }
+
+            it("while querying based on value of sub-object's key") {
+                val result = dataSource.connection.use {
+                    it.executeQuery("SELECT phone_numbers from contacts where phone_numbers @> '[{\"tag\":\"Home\"}]';").toList()
+                }
+
+                val queryAnalysis = dataSource.connection.use {
+                    it.executeQuery("EXPLAIN ANALYZE SELECT phone_numbers from contacts where phone_numbers @> '[{\"tag\":\"Home\"}]';;").toList()
+                }
+
+                println(">> " + queryAnalysis[0])
+                println("JSON_ARRAY_WITH_SUB_OBJECT_KEY_INDEX: Plan time = " + queryAnalysis[3])
+                println("JSON_ARRAY_WITH_SUB_OBJECT_KEY_INDEX: Execution time = " + queryAnalysis[4])
+
+                result.map { it["phone_numbers"] }.size shouldBe 10
+            }
+
+            it("selecting sub-object's particular key's value") {
+                 dataSource.connection.use {
+                    it.executeCommand("CREATE INDEX idx_phone_numbers_tag ON contacts USING gin ((phone_numbers->'tag')jsonb_path_ops);")
+                }
+
+                val result = dataSource.connection.use {
+                    it.executeQuery("""SELECT obj.val->>'value' as number
+                                           FROM   contacts
+                                           JOIN   LATERAL jsonb_array_elements(contacts.phone_numbers) obj(val) ON obj.val->>'tag' = 'Work'
+                                           WHERE  contacts.phone_numbers @> '[{"tag":"Work"}]';""").toList()
+                }
+
+                val queryAnalysis = dataSource.connection.use {
+                    it.executeQuery("EXPLAIN ANALYZE SELECT obj.val->>'value' as number FROM contacts JOIN LATERAL jsonb_array_elements(contacts.phone_numbers) obj(val) ON obj.val->>'tag' = 'Work' WHERE contacts.phone_numbers @> '[{\"tag\":\"Work\"}]';").toList()
+                }
+
+                println(">> " + queryAnalysis[0])
+                println("JSON_ARRAY_WITH_SUB_OBJECT_KEY_INDEX: Plan time = " + queryAnalysis[6])
+                println("JSON_ARRAY_WITH_SUB_OBJECT_KEY_INDEX: Execution time = " + queryAnalysis[7])
+
+                result.map { it["number"] }.distinct()[0] shouldBe "9876543210"
             }
         }
     }
